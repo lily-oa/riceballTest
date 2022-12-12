c3fbe8113e1b6ad18a3b95719f986860a0db8344
lily <a0976198586@gmail.com>
2022-12-12 19:08:01 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,40 +1,17 @@
-// Swal.fire({
-//   icon:'info',
-//   title: '請填寫文字內容',
-//   input: 'text',
-// }).then(result => {
-//   console.log(result.value);
-//   Swal.fire({
-//     icon:'success',
-//     title:'新增內容成功',
-//     text: result.value
-//   })
-// })
-
-
-
-
-
-
-const ipAPI = '//api.ipify.org?format=json'
-
-const inputValue = fetch(ipAPI)
-  .then(response => response.json())
-  .then(data => data.ip)
-
-const { value: ipAddress } = await Swal.fire({
-  title: 'Enter your IP address',
+Swal.fire({
+  icon:'info',
+  title: '請填寫文字內容',
   input: 'text',
-  inputLabel: 'Your IP address',
-  inputValue: inputValue,
-  showCancelButton: true,
-  inputValidator: (value) => {
-    if (!value) {
-      return 'You need to write something!'
-    }
-  }
+}).then(result => {
+  console.log(result.value);
+  Swal.fire({
+    icon:'success',
+    title:'新增內容成功',
+    text: result.value
+  })
 })
 
-if (ipAddress) {
-  Swal.fire(`Your IP address is ${ipAddress}`)
-}
+
+
+
+
