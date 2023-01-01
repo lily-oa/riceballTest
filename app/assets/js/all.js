71bac6b428a36ed233a50aff049c825eaf4d32cf
lily <a0976198586@gmail.com>
2023-01-01 17:54:51 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -26,7 +26,57 @@
 // const timeout4 = setTimeout(() => console.log('end'), 4000); 
 
 //---------------------------------------------------------------test2
-const checkScore = new Promise((resolve, reject) => {
-  const score = Math.round(Math.random()*100);
-  resolve(score);
-});
+// const checkScore = new Promise((resolve, reject) => {
+//   console.log('正在批改中')
+//   setTimeout(()=>{
+//    const score = Math.round(Math.random()*100);
+//    if(score >= 60){
+//     resolve(score);
+//    }else{
+//     reject('不及格');
+//    }
+//   }, 2000);
+  
+  
+// });
+
+// checkScore
+//   .then(data => console.log(data))  
+//   .catch(error => console.log(error))
+
+
+
+// const checkStuName = new Promise((resolve, reject) =>{
+//   console.log('正在確認名單');
+//   setTimeout(() => {
+//     const stuScore = Math.round(Math.random()*100);
+//     if(stuScore >= 60){
+//       resolve(stuScore);
+//     }else{
+//       reject(`${stuScore}這是要刪除的名單號`);
+//     }
+//   }, 5000);
+// });
+
+// checkStuName
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error));
+
+//------------------------------------------------------test3
+const checkScore = (score) =>{
+ return new Promise((resolve, reject) => {
+    console.log('正在觀察是否及格')
+    setTimeout(()=>{
+
+     if(score >= 60){
+      resolve(score);
+     }else{
+      reject('不及格');
+     }
+    }, 2000);
+  });
+}
+
+checkScore(59)
+  .then(data => console.log(data))  
+  .catch(error => console.log(error))
