c5834ff1164f01508d467c73a59681a32ba12a0c
lily <a0976198586@gmail.com>
2023-01-01 14:37:45 +0800

    Update 2023-01-01T06:37:43.693Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,15 +1,27 @@
 "use strict";
 
-Swal.fire({
-  icon: 'info',
-  title: '請填寫文字內容',
-  input: 'text'
-}).then(function (result) {
-  console.log(result.value);
-  Swal.fire({
-    icon: 'success',
-    title: '新增內容成功',
-    text: result.value
-  });
-});
+// Swal.fire({
+//   icon:'info',
+//   title: '請填寫文字內容',
+//   input: 'text',
+// }).then(result => {
+//   console.log(result.value);
+//   Swal.fire({
+//     icon:'success',
+//     title:'新增內容成功',
+//     text: result.value
+//   })
+// })
+
+//----------------------------------------------------------test
+var timeout = setTimeout(callFun, 1500);
+function callFun() {
+  console.log('觸發');
+}
+var timeout2 = setTimeout(function () {
+  return console.log('qq');
+}, 2000);
+var timeout3 = setTimeout(function () {
+  return console.log('你好');
+}, 3000);
 //# sourceMappingURL=all.js.map
