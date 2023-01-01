53bdc4b0b815ef83abb16242532b79522b5655fc
lily <a0976198586@gmail.com>
2023-01-01 14:23:13 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,17 +1,18 @@
-Swal.fire({
-  icon:'info',
-  title: '請填寫文字內容',
-  input: 'text',
-}).then(result => {
-  console.log(result.value);
-  Swal.fire({
-    icon:'success',
-    title:'新增內容成功',
-    text: result.value
-  })
-})
+// Swal.fire({
+//   icon:'info',
+//   title: '請填寫文字內容',
+//   input: 'text',
+// }).then(result => {
+//   console.log(result.value);
+//   Swal.fire({
+//     icon:'success',
+//     title:'新增內容成功',
+//     text: result.value
+//   })
+// })
 
 
+//----------------------------------------------------------test
 
 
 
