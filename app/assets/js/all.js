47cfe77abf22ceb1831ed0dd37edc9a675524c84
lily <a0976198586@gmail.com>
2023-01-03 21:24:10 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -226,8 +226,8 @@ const axios = {
     return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
       xhr.open('GET', url);
-      xhr.onload = resolve(xhr.responseText);
-      xhr.onerror = reject(xhr.statusText);
+      xhr.onload = () => resolve(xhr.responseText);
+      xhr.onerror = () => reject(xhr.statusText);
       xhr.send();
     })
   }
