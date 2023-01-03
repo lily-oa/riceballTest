bff6b28f0dc81a2fbdd053758222c60b2517984f
lily <a0976198586@gmail.com>
2023-01-03 11:56:03 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -190,9 +190,28 @@
 
 //-------------------------------------------------------------test7
 // Fetch
+// const url = 'https://raw.githubusercontent.com/hexschool/2021-ui-frontend-job/master/frontend_data.json';
+
+// fetch(url)
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error))
+
+//------------------------------------------------------------test8
+// XMLHttpRequest
 const url = 'https://raw.githubusercontent.com/hexschool/2021-ui-frontend-job/master/frontend_data.json';
 
-fetch(url)
-  .then(response => response.json())
+
+function getUrl(url) {
+  return new Promise((resolve, reject)=>{
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onload = () => resolve(xhr.responseText);
+    xhr.onerror = () =>reject(xhr.statusText);
+    xhr.send();
+  })
+}
+
+getUrl(url)
   .then(data => console.log(data))
-  .catch(error => console.log(error))
+  .catch(error => console.log(error));
