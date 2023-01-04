06afe576e1d752849a796c97428e52fe1509503b
lily <a0976198586@gmail.com>
2023-01-04 08:07:25 +0800

    Update 2023-01-04T00:07:23.455Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -232,23 +232,64 @@
 //   .catch(error => console.log(error));
 
 //----------------------------------------------------------------test10
-//sweetalert2
+//sweetalert2  彈跳視窗，此套件的載入方式是在 layout.ejs 使用它的 cdn
 
-Swal.fire({
-  title: 'Are you sure?',
-  text: "You won't be able to revert this!",
-  icon: 'warning',
-  showCancelButton: true,
-  confirmButtonColor: '#3085d6',
-  cancelButtonColor: '#d33',
-  confirmButtonText: 'Yes, delete it!'
-}).then(function (result) {
-  console.log(result);
-  if (result.isConfirmed) {
-    Swal.fire({
-      title: '新增成功',
-      icon: 'success'
-    });
-  }
+// Swal.fire({
+//   title: 'Are you sure?',
+//   text: "You won't be able to revert this!",
+//   icon: 'warning',
+//   showCancelButton: true,
+//   confirmButtonColor: '#3085d6',
+//   cancelButtonColor: '#d33',
+//   confirmButtonText: 'Yes, delete it!'
+// }).then((result) => {
+//   console.log(result);
+//   if (result.isConfirmed) {
+//     Swal.fire(
+//       {
+//         title: '新增成功',
+//         icon: 'success',
+//       }
+//     )
+//   }
+// })
+
+//------------------------------------------------------------------test11
+// sweetalert2 input Types
+// Swal.fire({
+//   icon: 'info',
+//   title: '請填寫文字內容',
+//   input: 'text'
+// }).then(result =>{
+//   console.log(result.value);
+//   Swal.fire({
+//     icon:'success', 
+//     title:'新增內容成功',
+//     text: result.value,
+//   })
+// })
+
+//-------------------------------------------------------------------test12
+var ipAPI = '//api.ipify.org?format=json';
+var inputValue = async(ipAPI).then(function (response) {
+  return response.json();
+}).then(function (data) {
+  return data.ip;
 });
+var _await$Swal$fire = await Swal.fire({
+    title: 'Enter your IP address',
+    input: 'text',
+    inputLabel: 'Your IP address',
+    inputValue: inputValue,
+    showCancelButton: true,
+    inputValidator: function inputValidator(value) {
+      if (!value) {
+        return 'You need to write something!';
+      }
+    }
+  }),
+  ipAddress = _await$Swal$fire.value;
+if (ipAddress) {
+  Swal.fire("Your IP address is ".concat(ipAddress));
+}
 //# sourceMappingURL=all.js.map
