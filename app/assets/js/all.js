473abc7b0c9c80c3fbf8ad0c6833b6878df06b88
lily <a0976198586@gmail.com>
2023-01-08 12:01:11 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -316,3 +316,8 @@ function signUp(email, nickname, pwd) {
   .then(res => console.log(res))
   .catch(error => console.log(error.response))
 }
+
+function getAPI(){
+  axios.get('https://data.kcg.gov.tw/dataset/6f29f6f4-2549-4473-aa90-bf60d10895dc/resource/30dfc2cf-17b5-4a40-8bb7-c511ea166bd3/download/lightrailtraffic.json')
+  .then(res => console.log(res));
+}
