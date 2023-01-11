f99151192ffe120e018ebdeddb9197ade9c2ac56
lily <a0976198586@gmail.com>
2023-01-12 07:44:29 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -360,34 +360,18 @@ function addTodo(todo){
 
 //編輯todo(單筆修改)
 function updateTodo(todo, todoId){
-  axios.put(`${apiUrl}/todos/${todoId}`,{
+  axios.put(`${apiUrl}/todos/${todoId}`, {
     "todo": {
       "content": todo
     }
   },{
     headers:{
-      "Authorization":token
+      'Authorization':token
     }
   })
-  .then(res =>console.log(res))
-  .catch(err =>consol.log(err))
+  .then(res => console.log(res))
+  .catch(err => console.log(err))
 }
-
-
-
-// function updateTodo(todo, todoId){
-//   axios.put(`${apiUrl}/todos/${todoId}`, {
-//     "todo": {
-//       "content": todo
-//     }
-//   },{
-//     headers:{
-//       'Authorization':token
-//     }
-//   })
-//   .then(res => console.log(res))
-//   .catch(err => console.log(err))
-// }
 
 
 //-------------------------------------------------------------------------------------------------測試是否有支援垮網域
