a3901f2a4812cee64539a46efb46b43fae0e2d60
lily <a0976198586@gmail.com>
2023-02-24 11:57:54 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -502,81 +502,96 @@
 
 //----------------------------------------------------我設計的範例 
 // 可不可以進去夜店
-// function wantToGo(yourName){
-//   return new Promise((resolve, reject) => {
-//     console.log('你有多少錢？');
-//     setTimeout(()=>{
-//       const money = Math.round(Math.random()*1000);
-//       if(money >= 300){    
-//         resolve(
-//           {
-//             yourName,
-//             money
-//           }
-//         )
-//       }else{
-//         reject('錢不夠，滾出去!!')
-//       }
-//     }, 3000);
-//   });
-// }
+function wantToGo(yourName){
+  return new Promise((resolve, reject) => {
+    console.log('你有多少錢？');
+    setTimeout(()=>{
+      const money = Math.round(Math.random()*1000);
+      if(money >= 300){    
+        resolve(
+          {
+            yourName,
+            money
+          }
+        )
+      }else{
+        reject('錢不夠，滾出去!!')
+      }
+    }, 3000);
+  });
+}
 
-// function canI(number){
-//   return new Promise((resolve, reject)=>{
-//     console.log('夠嗎？可以讓開了嗎!!!')
-//     setTimeout(() => {
-//       if(number.money >= 500){
-//         resolve(`${number.yourName}你可以直接升級成我們的永久會員喔~`)
-//       }else if(number.money >= 300 && number.money<500){
-//         resolve(`${number.yourName}，你只要再加50萬就可以成為白金會員喔~每次來都能打8折`)
-//       }
-//     }, 2000)
-//   });
-// }
+function canI(number){
+  return new Promise((resolve, reject)=>{
+    console.log('夠嗎？可以讓開了嗎!!!')
+    setTimeout(() => {
+      if(number.money >= 500){
+        resolve(`${number.yourName}你可以直接升級成我們的永久會員喔~`)
+      }else if(number.money >= 300 && number.money<500){
+        resolve(`${number.yourName}，你只要再加50萬就可以成為白金會員喔~每次來都能打8折`)
+      }
+    }, 2000)
+  });
+}
 
 // wantToGo('親愛的~')
 //   .then(number => canI(number))
 //   .then(go => console.log(go))
 //   .catch(error => console.log(error))
+  
+  
+
+  // async await
+  const init = async function(){
+    try{
+      const someOne = await wantToGo('親愛的~');
+      const into = await canI(someOne);
+      console.log(into);
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  init();
 
 
 //---------------------------------async await非同步的語法
-function correctTest(name){
-  return new Promise((resolve, reject) =>{
-    console.log('批改作業中');
-    setTimeout(()=>{
-      const score = Math.round(Math.random()*100);
-      if(score >= 20){
-        resolve(
-          {
-            name, 
-            score
-          }
-        )
-      }else{
-        reject("您已達退學的門檻");
-      }
+// function correctTest(name){
+//   return new Promise((resolve, reject) =>{
+//     console.log('批改作業中');
+//     setTimeout(()=>{
+//       const score = Math.round(Math.random()*100);
+//       if(score >= 20){
+//         resolve(
+//           {
+//             name, 
+//             score
+//           }
+//         )
+//       }else{
+//         reject("您已達退學的門檻");
+//       }
       
       
-    }, 2000);
-  })
-}
+//     }, 2000);
+//   })
+// }
 
-function checkReward(data){
-  return new Promise((resolve, reject) => {
-    console.log('檢查獎品中');
-    setTimeout(()=>{
-      if(data.score >= 90){
-        resolve(`${data.name}獲得電影票`)
-      }else if(data.score>=60 && data.score<90){
-        resolve(`${data.name}獲得嘉獎`)
-      }else{
-        console.log(data.score);
-        reject(`你沒有獎品打手心10下`)
-      }
-    }, 1000)
-  })
-}
+// function checkReward(data){
+//   return new Promise((resolve, reject) => {
+//     console.log('檢查獎品中');
+//     setTimeout(()=>{
+//       if(data.score >= 90){
+//         resolve(`${data.name}獲得電影票`)
+//       }else if(data.score>=60 && data.score<90){
+//         resolve(`${data.name}獲得嘉獎`)
+//       }else{
+//         console.log(data.score);
+//         reject(`你沒有獎品打手心10下`)
+//       }
+//     }, 1000)
+//   })
+// }
 
 // correctTest('小明')
 //   .then(data => checkReward(data))
@@ -591,16 +606,16 @@ function checkReward(data){
 // async 是非同步的語法
 // await 是 then(){retrun}的意思，跑 promise
 // init 初始化函式
-const init = async function(){
-  try{
-    const studentA = await correctTest('小明');
-    // 過一秒後才執行下段語法
-    // 這邊要暫停一下等它回傳我才執行下面的code
-    const rewardA = await checkReward(studentA);
-    console.log(rewardA);
-  }catch(error){
-    console.log(error)
-  }
-}
-//執行函式
-init();
+// const init = async function(){
+//   try{
+//     const studentA = await correctTest('小明');
+//     // 過一秒後才執行下段語法
+//     // 這邊要暫停一下等它回傳我才執行下面的code
+//     const rewardA = await checkReward(studentA);
+//     console.log(rewardA);
+//   }catch(error){
+//     console.log(error)
+//   }
+// }
+// //執行函式
+// init();
